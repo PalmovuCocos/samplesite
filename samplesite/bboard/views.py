2a029587e3e947235313a537a4a990b11bd151e3
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-10-26 23:38:22 +0300

    добавление представления index и запись его url

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    '''
+    :param request: это экземпляр класса HttpRequest
+    он хранит различные сведения о полученом запросе: запрашиваемый интернет-адрес, данные клиента, служебную ин-ю и т д
+    :return:
+    '''
+
+    return HttpResponse("Здесь будет выведен список объявлений.")
