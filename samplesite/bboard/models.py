47002eeb5725c5f7d1794e3de8173801413685f5
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-10-27 00:06:55 +0300

    создание таблицы Bb и выполнение миграции

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Bd(models.Model):
+    title = models.CharField(max_length=50)
+    # null - будет хранить в бд значения Null
+    # blank - можно не заполнять данное поле
+    content = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    # db_index - создает индекс (дальше мы будем по нему проводить сортировку)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
