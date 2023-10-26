6c82e7f0ca11c902e5a065026b833478f0bfca0f
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-10-26 23:47:27 +0300

    добавление иерархии маршрутизации для приложения bboar

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from bboard.views import index
 
 urlpatterns = [
-    path('bboard/', index),
+    path('bboard/', include('bboard.urls')), # добавляем вложеный список маршрутов вторым аргументом
     path('admin/', admin.site.urls),
 ]
