ccf29d065358eafce1a04ed959db65fa4296e7e8
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-11-01 15:17:00 +0300

    вывод информации о Bd в index.html

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.template import loader
+
 from .models import *
 
 
 def index(request):
-
     '''
     :param request: это экземпляр класса HttpRequest
     он хранит различные сведения о полученом запросе: запрашиваемый интернет-адрес, данные клиента, служебную ин-ю и т д
     :return:
     '''
-
-    s = 'Список объявлений'
-    for bb in Bd.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    # загрузка шаблона (возвращает объект класса Template)
+    template = loader.get_template('bboard/index.html')
+    # взятие всех записей
+    bbs = Bd.objects.order_by('-published')
+    # формирование словаря content для передачи в httpResponse
+    content = {'bbs': bbs}
+    # выполнение рендеринга шаблона с помощью метода render
+    # в итоне в httpResponse поступает html строка
+    return HttpResponse(template.render(content, request))
