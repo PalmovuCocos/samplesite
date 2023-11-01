e32facd8ccc6d8d936977f610ec86221a2ab9a02
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-11-01 16:19:19 +0300

    добавление класса-редактора в admin.py

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from bboard.models import Bd
 
-admin.site.register(Bd)
+
+class BdAdmin(admin.ModelAdmin):  # класс-редактор
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content', )
+
+
+admin.site.register(Bd, BdAdmin)
