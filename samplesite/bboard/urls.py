6a265c4ecc011f540dfe31fd821d63e8fc859f61
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-11-02 20:55:08 +0300

    Добавление в urls имен и замена ссылок в шаблонах

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from bboard.views import index, by_rubric
 
 urlpatterns = [
-    path('', index),
-    path('<int:rubric_id>/', by_rubric) # параметризованный маршрут
+    # name задаем для того, чтобы в шалонах можно было бы прописывать имена, вместо маршрутов
+    # т.к. если поменяется utl у пути, то можно будет не менять их в шаблонах
+    path('', index, name='index'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric')  # параметризованный маршрут
 ]
