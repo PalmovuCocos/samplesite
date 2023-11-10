470c5e64f6d18b70957d9ba4fb797eb92cc1c6b5
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-11-11 00:28:23 +0300

    добавление комментариев в настройки сайта, изменение подключения bboard

--- samplesite/bboard/apps.py
+++ samplesite/bboard/apps.py
@@ -3,4 +3,6 @@ from django.apps import AppConfig
 
 class BboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bboard'
+    name = 'bboard'    # полный путь к пакету приложения относительно папки проекта
+    label = 'bboard'    # псевдоним приложения
+    verbose_name = 'Доска объявлений'   # данное название будет отображаться в admin'ке сайта
