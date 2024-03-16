4577037630affae2133fe2f3688e3c1a782c90a3
Dmitry <cocos.barchuck@yandex.ru>
2024-03-16 20:01:39 +0300

    добавление текстового поля choices и новой таблицы с использованием choices, но с значениями float, а также добавление related_name у fk

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -2,9 +2,28 @@ from django.db import models
 
 
 class Bd(models.Model):
+    # KINDS = (
+    #     (None, 'Выбирете тип публикуемого объявления'),
+    #     ('Купля-продажа', (
+    #         ('b', 'Куплю'),
+    #         ('s', 'Продам'),
+    #     )),
+    #     ('Обмен', (
+    #         ('c', 'Обменяю'),
+    #     ))
+    # )
+    class Kind(models.TextChoices):
+        BUY = 'b', 'Куплю'
+        SELL = 's', 'Продам'
+        EXCHANGE = 'c', 'Обменяю'
+        RENT = 'r'
+        __empty__ = 'Выбирете тип публикуемого объявления'
+
+    kind = models.CharField(max_length=1, choices=Kind.choices, default=Kind.SELL, verbose_name="Вид")
     title = models.CharField(max_length=50, verbose_name="Товар")
     # значение protect защищает записи от каскадного удаления
-    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name="Рубрика")
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT,
+                               verbose_name="Рубрика", related_name='entries')
     # null - будет хранить в бд значения Null
     # blank - можно не заполнять данное поле
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
@@ -30,3 +49,10 @@ class Rubric(models.Model):
         ordering = ['name']
 
 
+class Measuere(models.Model):
+    class Measurements(float, models.Choices):
+        METERS = 1.0, 'Метры'
+        FEET = 0.3048, 'Футы'
+        YARDS = 0.9144, 'Ярды'
+
+    measurement = models.FloatField(choices=Measurements.choices)
